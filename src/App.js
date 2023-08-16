f45a1e53a5ea512f40794a9425057e77306ccf43
umeshpatil98 <up28511@gmail.com>
2023-08-16 09:56:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Component/Header/Header.jsx';
+import Home from './Component/Home/Home.jsx';
 
 function App() {
   return (
     <div className='App'>
       <Header/>
+      <Home/>
     </div>
   );
 }
---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () =>{
+    return (
+        <p>Hello From Home</p>
+    )
+
+     
+} 
+
+export default Home;
