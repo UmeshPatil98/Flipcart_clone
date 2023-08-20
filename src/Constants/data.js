6bcbf1a5c838bdeaa91d457e2d750b421171bd94
umeshpatil98 <srinupatil98@gmail.com>
2023-08-20 09:28:17 +0530

    second commit

--- src/Constants/data.js
+++ src/Constants/data.js
@@ -9,6 +9,5 @@ export const navData = [
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/ee162bad964c46ae.png?q=100', text: 'Home' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100', text: 'Appliances' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100', text: 'Travel' },
-    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100', text: 'Travel' },
     { url: 'https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100', text: 'Beauty, Toys & More' }
 ];
